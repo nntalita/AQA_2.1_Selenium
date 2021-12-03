489708aea79aa06690b94371740525d70f9f01f5
Natalya Pletneva <nntalita@gmail.com>
2021-12-03 22:18:57 +0300

    format and optimize

--- src/test/java/ru/netology/form/FormTest.java
+++ src/test/java/ru/netology/form/FormTest.java
@@ -1,34 +1,28 @@
 package ru.netology.form;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.checkerframework.checker.units.qual.C;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FormTest {
 
- private WebDriver driver;
+    private WebDriver driver;
 
     @BeforeAll
     public static void setUpAll() {
-//        System.setProperty("webdriver.chrome.driver", "./driver/win/chromedriver.exe");
-
-    WebDriverManager.chromedriver().setup();
+        WebDriverManager.chromedriver().setup();
     }
 
     @BeforeEach
     public void setUp() {
-        ChromeOptions options =new ChromeOptions();
+        ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
@@ -48,8 +42,6 @@ public class FormTest {
 
         driver.findElement(By.cssSelector("[data-test-id=\"name\"] input")).sendKeys("Арчи Марчи");
         driver.findElement(By.cssSelector("[data-test-id=\"phone\"] input")).sendKeys("+79024560869");//        List<WebElement> textfields = driver.findElements(By.className("input__control"));
-//        textfields.get(0).sendKeys("Арчи Марчи");
-//        textfields.get(1).sendKeys("+79024567896");
         driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.tagName("button")).click();
         String actualAnswer = driver.findElement(By.className("Success_successBlock__2L3Cw")).getText().trim();
